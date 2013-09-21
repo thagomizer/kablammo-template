aa6fc9a898a83224ac9b06b0b2188ef64bf44d5d
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-09-21 11:15:27 -0700

    Fire at enemies!
    RAWR!

--- strategy.rb
+++ strategy.rb
@@ -1,14 +1,14 @@
 # Game Info
-# 
+#
 # board -- the game board
 #   See: https://github.com/carbonfive/kablammo-strategy/blob/master/lib/strategy/models/board.rb
 # robot -- you
 #   See: https://github.com/carbonfive/kablammo-strategy/blob/master/lib/strategy/models/robot.rb
 # opponents -- opponent robots
 #   See: https://github.com/carbonfive/kablammo-strategy/blob/master/lib/strategy/models/robot.rb
-# 
+#
 # Actions:
-# 
+#
 # fire!(skew=0) -- shot at something
 # rotate!(degrees) -- rotate the turret
 # rest -- rest and reload
@@ -18,5 +18,19 @@
 # move_west! -- move west
 
 on_turn do
-  fire!
+# If out of ammo
+  if ammo < 1
+
+   # Run away and hide
+
+  else
+    # Find closest apponent
+    enemy = opponents.reject { |o| can_see?(o) }.min{ |o| distance_to(o) }
+
+    if aiming_at?(enemy)
+      fire!
+    else
+      aim_at!(enemy)
+    end
+  end
 end
